25b0b431ed171257c7c8b82e2172eaed5993ffb2
Stefan Trinh <stefantrinh1.work@gmail.com>
2018-08-16 20:46:29 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// Jquery Required and absolute positioning in css
+// keycode Notes
+// 37 = arrowleft  | 38 = arrowup
+// 39 = arrowright | 40 = arrowdown
+var logo1 = document.querySelector('img')
+var $theobject = $('img') // this is the object you like to operated on
+// the below catches the keyboard keys pressed
+var $html = $("html")
+$html.on("keydown", function(e) {
+  if(e.keyCode === 37) {objectmove(37);}
+  else if (e.keyCode === 39) {objectmove(39);}
+  else if (e.keyCode === 40) {objectmove(40);}
+  else if (e.keyCode === 38) {objectmove(38);}
+})
+// The clicks functions are using 4 buttons in the html via ID to move the object around
+$("#up").click(function() {objectmove(38);})
+$("#down").click(function(){objectmove(40);})
+$("#left").click(function(){objectmove(37);})
+$("#right").click(function(){objectmove(39);})
+// below is the function that alters the css to move the object
+function objectmove(keyCode){
+  if (keyCode === 37){
+    var current = $theobject.offset().left;
+    $theobject.css("left", current -10);}
+  else if (keyCode === 39){
+    var current = $theobject.offset().left
+    $theobject.css("left", current +10);}
+  else if (keyCode === 40){
+    var current = $theobject.offset().top
+    $theobject.css("top", current +10);}
+  else if (keyCode === 38){
+    var current = $theobject.offset().top
+    $theobject.css("top", current -10);}
+}
